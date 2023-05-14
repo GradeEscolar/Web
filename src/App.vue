094453d7e896fdf8ea40272dcd04a24ecf1529a8
gersonmsj <gerson.msj@gmail.com>
2023-05-13 22:50:12 -0300

    SPA

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/cadastro">Cadastro</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div v-if="page == 'Home'">
+    <Home @go-to-page="goToPage" />
+  </div>
+  <div v-if="page == 'About'">
+    <About @go-to-page="goToPage" />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Home from './components/Home.vue';
+import About from './components/About.vue';
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default defineComponent({
+  name: 'App',
+  components: {
+    Home,
+    About
+  },
+  data(): { 
+    page: string 
+  } {
+    return {
+      page: 'Home'
+    }
+  },
+  methods: {
+    goToPage(page: string){
+      this.page = page;
+    }
+  },
+  mounted() {
+    let page = localStorage.getItem('page');
+    if(page){
+      this.page = page;
+    }
+  },
+  watch: {
+    page(newPage: string){
+      localStorage.setItem('page', newPage);
+    }
+  }
+});
+</script>
